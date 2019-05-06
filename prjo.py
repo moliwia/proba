f0de4126bf18149cfd3112c444b5b19d25bacded
pawo161 <48882583+pawo161@users.noreply.github.com>
2019-05-06 14:07:49 +0200

    Add files via upload

---
+++ prjo.py
@@ -0,0 +1,13 @@
+def zmieniacz_x(ciag):
+    lista = list(ciag)
+    count = 0
+    for i in lista:
+        count+=1
+        if i == 'a':
+            index = int(count)
+            lista[index] = "X"
+    ciag="".join(lista)
+    return ciag
+
+znak = input("Proszę wpisać słowo do zmiany: ")
+print(zmieniacz_x(znak))
